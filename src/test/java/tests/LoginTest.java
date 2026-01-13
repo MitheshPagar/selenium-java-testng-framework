4accefd44b1d4cac3f7d729471bd1d028e2aeafa
Mithesh Pagar <pagarmithesh@gmail.com>
2026-01-13 20:04:22 +0530

    Initial Selenium framework with POM and TestNG

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import dev.failsafe.internal.util.Assert;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void validLoginTest() {
+        LoginPage login = new LoginPage(driver);
+        login.login("Admin", "admin123");
+        Assert.isTrue(driver.getTitle().equals("OrangeHRM"), "Login failed - Title does not match");
+    }
+}
