d65b0a83331098c4f1e0c67a55d07f1b859c9ab1
Mithesh Pagar <pagarmithesh@gmail.com>
2026-01-20 18:47:17 +0530

    Added explicit Waits to the code

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,9 +3,12 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import utils.WaitUtils;
+
 public class LoginPage {
 
     WebDriver driver;
+    WaitUtils wait;
 
     By username = By.name("username");
     By password = By.name("password");
@@ -13,11 +16,17 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        wait = new WaitUtils(driver);
     }
 
     public void login(String user, String pass) {
+
+        wait.WaitByVisibility(username);
         driver.findElement(username).sendKeys(user);
+
         driver.findElement(password).sendKeys(pass);
+
+        wait.WaitForClickability(loginBtn);
         driver.findElement(loginBtn).click();
     }
 }
